0c4a483069aa49b25913be93c02af64744b84970
haixuanTao <tao.xavier@outlook.com>
2024-01-30 18:00:33 +0100

    Simplifying planning operator

--- operators/planning_op.py
+++ operators/planning_op.py
@@ -1,17 +1,47 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# forward-backward: [-1,1]
+X = 0
+# left-right: [-1,1]
+Y = 0
+# rotation: [-1800,1800]
+Z = 1000
+# xy speed in m/s: [0.5, 2]
+XY_SPEED = 1
+# z rotation speed in °/s: [10, 540]
+Z_SPEED = 10
+# pitch-axis angle in degrees: [-55, 55]
+GIMBAL_PITCH = 0
+# yaw-axis angle in degrees: [-55, 55]
+GIMBAL_YAW = 0
+GOAL_OBJECTIVES = [X, Y, Z]
+GIMBAL_POSITION_GOAL = [GIMBAL_PITCH, GIMBAL_YAW]
 
 from typing import Callable, Optional, Union
 
-from time import sleep
-from enum import Enum
 import numpy as np
 import pyarrow as pa
-from utils import LABELS
 from dora import DoraStatus
 
+
+CAMERA_WIDTH = 960
 CAMERA_HEIGHT = 540
-DISTANCE = 2
+
+
+def do_rectangles_overlap(rect1, rect2):
+    """
+    Check if two rectangles overlap.
+    Each rectangle is defined by two points (x1, y1, x2, y2)
+    where (x1, y1) is the top left corner, and (x2, y2) is the bottom right corner.
+    """
+    # Extract coordinates
+    [x11, y11, x12, y12] = rect1
+    [x21, y21, x22, y22] = rect2
+
+    # Check for overlap
+    return not (x12 < x21 or x22 < x11 or y12 < y21 or y22 < y11)
+
+
+def estimated_distance(y):
+    return ((12 * 22) / (y - (CAMERA_HEIGHT / 2))) / 2.77 - 0.08
 
 
 class Operator:
@@ -22,82 +52,120 @@ class Operator:
     def __init__(self):
         self.over = False
         self.start = False
+        self.position = [0, 0, 0]
+        self.gimbal_position = [0, 0]
 
     def on_event(
         self,
         dora_event: dict,
         send_output: Callable[[str, Union[bytes, pa.Array], Optional[dict]], None],
     ) -> DoraStatus:
-        if dora_event["type"] == "INPUT":
-            return self.on_input(dora_event, send_output)
+        if dora_event["type"] != "INPUT":
+            return DoraStatus.CONTINUE
+
+        if dora_event["id"] == "position":
+            [x, y, z, gimbal_pitch, gimbal_yaw] = dora_event["value"].to_numpy()
+            self.position = [x, y, z]
+            self.gimbal_position = [gimbal_pitch, gimbal_yaw]
+
+            direction = np.array(GOAL_OBJECTIVES) - np.array(self.position)
+            print("position ", dora_event["value"].to_numpy(), flush=True)
+            print(direction, flush=True)
+            if any(abs(direction) > 0.1):
+                if abs(direction[0]) > 0.1:
+                    x = direction[0]
+                else:
+                    x = 0
+                if abs(direction[1]) > 0.1:
+                    y = direction[1]
+                else:
+                    y = 0
+                if abs(direction[2]) > 0.1:
+                    z = direction[2]
+                else:
+                    z = 0
+
+                print("control ", x, y, flush=True)
+                send_output(
+                    "control",
+                    pa.array([x, y, 0, XY_SPEED, Z_SPEED]),
+                    dora_event["metadata"],
+                )
+
+            if (
+                abs(gimbal_pitch - GIMBAL_PITCH) > 0.2
+                or abs(gimbal_yaw - GIMBAL_YAW) > 0.2
+            ):
+                send_output(
+                    "gimbal_control",
+                    pa.array([GIMBAL_PITCH, GIMBAL_YAW, 20, 20]),
+                    dora_event["metadata"],
+                )
+
         return DoraStatus.CONTINUE
 
-    def on_input(
-        self,
-        dora_input: dict,
-        send_output: Callable[[str, Union[bytes, pa.array], Optional[dict]], None],
-    ) -> DoraStatus:
-        if dora_input["id"] == "position":
-            [x, y, z] = dora_input["value"].to_numpy()
-            print(x, y, z, flush=True)
-            if x > DISTANCE:
-                send_output("led", pa.array([0, 0, 255]), dora_input["metadata"])
-                send_output("stop", pa.array([""]), dora_input["metadata"])
-                return DoraStatus.CONTINUE
-        elif dora_input["id"] == "bbox":
+
+a = """
+
+        elif dora_event["id"] == "bbox":
             if not self.start:
-                send_output("led", pa.array([255, 0, 0]), dora_input["metadata"])
+                send_output("led", pa.array([255, 0, 0]), dora_event["metadata"])
                 self.start = True
             blaster = 0
             x, y, z, acc = 0, 0, 0, 0
-            bboxs = dora_input["value"].to_numpy()
+            bboxs = dora_event["value"].to_numpy()
             bboxs = np.reshape(bboxs, (-1, 6))
-            arrays = pa.array([0, 0, 0, 0])
             obstacle = False
-            box = False
-            for bbox in bboxs:
-                box = True
-                [
-                    min_x,
-                    min_y,
-                    max_x,
-                    max_y,
-                    confidence,
-                    label,
-                ] = bbox
-                if LABELS[int(label)] == "ABC":
-                    continue
+            if bboxs.len() > 0:
+                for bbox in bboxs:
+                    [
+                        min_x,
+                        min_y,
+                        max_x,
+                        max_y,
+                        confidence,
+                        label,
+                    ] = bbox
 
-                if (
-                    (min_x + max_x) / 2 > 385
-                    and (min_x + max_x) / 2 < 575
-                    and LABELS[int(label)] == "bottle"
-                ):
-                    blaster = 128
-                d = ((12 * 22) / (max_y - (CAMERA_HEIGHT / 2))) / 2.77 - 0.08
-                if d < 0.5 and (
-                    (max_x > 385 and min_x <= 385)
-                    or (min_x < 575 and max_x >= 575)
-                    or (min_x < 385 and max_x > 575)
-                    or (min_x > 385 and max_x < 575)
-                ):
-                    if (min_x + max_x) / 2 > 470:
-                        y = -0.15
-                        acc = 0.4
-                    elif (min_x + max_x) / 2 <= 470:
-                        y = 0.15
-                        acc = 0.4
+                    x_center = (min_x + max_x) / 2
+                    if LABELS[int(label)] == "ABC":
+                        continue
 
-                    obstacle = True
-                    break
-            if obstacle == False and box == True:
-                x = 0.2
-                acc = 0.6
-            arrays = pa.array([x, y, z, acc])
+                    # Blast light into bottle if it's in the middle
+                    if (
+                        abs(x_center - CAMERA_WIDTH / 2) < 100
+                        and LABELS[int(label)] == "bottle"
+                    ):
+                        blaster = 128
 
-            if box == True:
-                send_output("blaster", pa.array([blaster]), dora_input["metadata"])
-                send_output("control", arrays, dora_input["metadata"])
-                blaster = 0
+                    overlap_planning = do_rectangles_overlap(
+                        [
+                            min_x,
+                            estimated_distance(max_y),
+                            max_x,
+                            estimated_distance(min_y),
+                        ],
+                        [CAMERA_WIDTH / 2 - 100, 0, CAMERA_WIDTH / 2 + 100, 0.5],
+                    )
+                    # Computed depth based on expirmental measurement of the bottom of the rectangle
+                    if overlap_planning:
+                        if x_center > CAMERA_WIDTH / 2:
+                            y = -0.15
+                            acc = 0.4
+                        else:
+                            y = 0.15
+                            acc = 0.4
+
+                        obstacle = True
+
+                        break
+                if obstacle == False:
+                    x = 0.2
+                    acc = 0.6
+                arrays = pa.array([x, y, z, acc])
+
+                send_output("blaster", pa.array([blaster]), dora_event["metadata"])
+                send_output("control", arrays, dora_event["metadata"])
 
         return DoraStatus.CONTINUE
+"""
